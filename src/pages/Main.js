7c9425138ddb95ffe1957f47d51eceb4a8a35949
jordeva.silva <jordevalucas@hotmail.com>
2020-04-16 14:27:22 -0300

    add info of marker of dev and styles

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -37,6 +37,18 @@ function Main() {
     return (
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -6.4122329, longitude: -35.1811169 }}>
+                <Image
+                    style={styles.avatar} 
+                    source={{ uri: 'https://avatars2.githubusercontent.com/u/33496399?s=460&u=13845319fdcd3a86bb7089cd3d533604e7203efa&v=4' }}
+                />
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Jordeva Lucas</Text>
+                        <Text style={styles.devBio}>Graduando de TI pela Universidade Federal do Rio Grande do Norte e bolsista Desenvolvedor WEB Front-End da Diretoria de TI do IMD</Text>
+                        <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     )
@@ -45,6 +57,33 @@ function Main() {
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5
     }
 })
 
